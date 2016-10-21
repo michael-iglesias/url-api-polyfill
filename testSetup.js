c19ff029a11890e61b83402564255841b66ddd41
Michael Iglesias <mike.iglesias11@gmail.com>
2016-10-21 12:15:12 -0400

    first commit

---
+++ testSetup.js
@@ -0,0 +1,16 @@
+// Tests are placed alongside files under test.
+// This file does the following:
+// 1. Sets the environment to 'test' so that
+//    dev-specific babel config in .babelrc doesn't run.
+// 2. Disables Webpack-specific features that Mocha doesn't understand.
+// 3. Registers babel for transpiling our code for testing.
+
+// This assures the .babelrc dev config (which includes
+// hot module reloading code) doesn't apply for tests.
+// Setting NODE_ENV to test instead of production because setting it to production will suppress error messaging
+// and propType validation warnings.
+process.env.NODE_ENV = 'test';
+
+// Register babel so that it will transpile ES6 to ES5
+// before our tests run.
+require('babel-register')();
